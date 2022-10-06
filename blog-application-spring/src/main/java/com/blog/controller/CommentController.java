8159e625ed720362838c8a6427c3c6bb1f615291
Ram <ss4033167@gmail.com>
2022-10-06 16:16:40 +0530

    Fix: response entity

--- blog-application-spring/src/main/java/com/blog/controller/CommentController.java
+++ blog-application-spring/src/main/java/com/blog/controller/CommentController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +22,35 @@ import com.blog.service.CommentService;
 public class CommentController {
 	@Autowired
 	private CommentService commentservice;
-	
+
 	@GetMapping("/posts/{postId}/comments")
-	public ResponseEntity<List<Comment>> getAllCommentsHandler(Integer postId){
+	public ResponseEntity<List<Comment>> getAllCommentsHandler(@PathVariable Integer postId) {
 		List<Comment> comments = commentservice.getAllCommentOfPost(postId);
-		return new ResponseEntity<List<Comment>>(comments,HttpStatus.OK);
+		return new ResponseEntity<List<Comment>>(comments, HttpStatus.OK);
+	}
+
+	@GetMapping("/posts/{postId}/comments/{id}")
+	public ResponseEntity<Comment> getCommentsHandler(@PathVariable Integer postId, @PathVariable Integer Id) {
+		Comment comments = commentservice.getCommentById(Id);
+		return new ResponseEntity<>(comments, HttpStatus.OK);
+	}
+
+	@PostMapping("/posts/{postId}/comments")
+	public ResponseEntity<Comment> newCommentHandler(@PathVariable Integer postId, @RequestBody Comment comment) {
+		Comment c = commentservice.newComment(comment);
+		return new ResponseEntity<>(c, HttpStatus.ACCEPTED);
+	}
+
+	@PutMapping("/posts/{postId}/comments/{id}")
+	public ResponseEntity<Comment> updateCommentHandler(@PathVariable Integer postId, @PathVariable Integer id,
+			@RequestBody Comment comment) {
+		Comment c = commentservice.updateComment(id, comment);
+		return new ResponseEntity<>(c, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/posts/{postId}/comments/{id}")
+	public ResponseEntity<Comment> deleteCommentHandler(@PathVariable Integer postId, @PathVariable Integer id) {
+		Comment c = commentservice.deleteComment(id);
+		return new ResponseEntity<>(c, HttpStatus.OK);
 	}
 }
--- blog-application-spring/src/main/java/com/blog/repository/CommentDAO.java
+++ blog-application-spring/src/main/java/com/blog/repository/CommentDAO.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.blog.model.Comment;
 
-
+@Repository
 public interface CommentDAO extends JpaRepository<Comment, Integer> {
 	@Query("from Comment as c where c.post.id=:id")
 	public List<Comment> findAllByPostId(@Param("id") Integer id);
